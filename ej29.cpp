df6bf91ca9ea3983b98e586ea083a4c169275b83
Joaquin <joaquin.nunezprado@ucsp.edu.pe>
2023-03-29 05:29:33 -0500

    nuevo

---
+++ ej29.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int var;
+    var = 0;
+    cout << var << '\t' << var*var << '\t' << var*var*var << "\n";
+    var = 1;
+    cout << var << '\t' << var*var << '\t' << var*var*var << "\n";
+    var = 2;
+    cout << var << '\t' << var*var << '\t' << var*var*var << "\n";
+    var = 3;
+    cout << var << '\t' << var*var << '\t' << var*var*var << "\n";
+    var = 4;
+    cout << var << '\t' << var*var << '\t' << var*var*var << "\n";
+
+}
